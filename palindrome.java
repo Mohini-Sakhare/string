2df651113da23e85a98f1e38ce7ac74d4078d7a1
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-08-31 23:26:23 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,24 @@
+
+//Check if a string is a palindrome or not
+//g.
+public class palindrome 
+{
+  public static boolean ispalindrome(String str)
+  {
+    for(int i=0; i<str.length()/2; i++)
+    {
+        int n= str.length();
+        if(str.charAt(i)!=str.charAt(n-1-i))
+        {
+           //not a palindrome
+           return false;  
+        }
+    }
+    return true;
+  }
+  public static void main(String args[])
+  {
+    String str="racecar";
+    System.out.println(ispalindrome(str));
+  }  
+}
